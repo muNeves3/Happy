d7c00ba28bf853b728ad1a35e0f24c5ac9fc7dc4
muNeves3 <murilogrilosn@gmail.com>
2020-10-12 16:33:58 -0300

    :rocket: dark/light theme

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import {FiPlus, FiMoon } from 'react-icons/fi';
+import {FiPlus, FiMoon, FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../../Images/map-marker.svg';
 import './styles.css';
 import 'leaflet/dist/leaflet.css'
+import light from '../../Styles/themes/light';
+import { map } from 'leaflet';
 
 function OrphanagesMap() {
+    var [mapTheme, setMapTheme] = useState('light')
+
+   function toggleMapTheme() {
+        if(mapTheme === "light") {
+            setMapTheme("dark")
+        }
+        else {
+            setMapTheme("light")
+        }
+   }
+    
     return(
         <div id="page-map">
             <aside>
@@ -22,7 +35,11 @@ function OrphanagesMap() {
                         <strong>Londrina</strong>
                         <span>Paraná</span>
                     </footer>
-            </aside>
+
+                    <Link to="/" className="back-to-home">
+                        <FiArrowLeft size={32} color="#fff"/>
+                    </Link>
+            </aside>    
           
             <Map 
                 center={[-23.2882176,-51.1868928]}
@@ -30,11 +47,11 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                    url={`https://api.mapbox.com/styles/v1/mapbox/${mapTheme}-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
             </Map>
 
-            <button className="dark-theme">
+            <button className="dark-theme" onClick={toggleMapTheme}>
                 <FiMoon  size={32} color="#fff"/>
             </button>
 
